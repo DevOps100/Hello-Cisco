bef8f8aa3fcfe63dcd518b4b7b585a7cfa6524db
DevOps100 <parmita.saha@zensar.com>
2017-10-31 14:47:36 +0530

    Update hello.groovy

--- hello.groovy
+++ hello.groovy
@@ -1,1 +1,14 @@
-"println 'Hello, World!'"
+for (item in Hudson.instance.items) {
+  notification = item.properties.find { it.getKey().getClass() == com.tikal.hudson.plugins.notification.HudsonNotificationPropertyDescriptor }   
+  if (notification != null) {
+    continue
+  }
+ 
+  println(">>>>>>>> Adding notification plugin to $item.name")  
+  protocol = com.tikal.hudson.plugins.notification.Protocol.UDP
+  endpoint = new com.tikal.hudson.plugins.notification.Endpoint(protocol, '172.16.24.204:11337')
+  notification = new com.tikal.hudson.plugins.notification.HudsonNotificationProperty([endpoint])
+ 
+  item.addProperty(notification)
+  item.save()
+}
